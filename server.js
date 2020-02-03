2c056960f25e8d90e376bdb8ff894c3216ab5c1a
shania <shania.dhani46@cuny.edu>
2020-02-02 20:52:37 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// locally hosted at http://localhost:8080
-
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
+  res.send("main page");
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+
+console.log('This is after the read call');
+
+app.listen(3000, ()=> console.log('App listening on port 3000'));
