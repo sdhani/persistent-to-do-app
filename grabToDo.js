e5e4638dad7606d707244e9b21535a22e60132e1
shania <shania.dhani46@cuny.edu>
2020-02-02 21:04:44 -0500

    fixed error in local render

--- grabToDo.js
+++ grabToDo.js
@@ -1,42 +1,25 @@
 'use strict';
-
 const fs = require('fs'); // load fs module to this application
+
 
 fs.readFile('data.json', (err, data) => { 
   if (err) throw err;
   let toDoList = JSON.parse(data);
   console.log(toDoList);
-  var source = '{{#each data}}<li>{{this.name}}</li>{{/each}}';
-  var template = handlebars.compile(source);
-  var outputString = template(data);
-  console.log(outputString);
 })
 
 console.log('This is after the read call');
 
 
 /* 
-  Note: 
+  Notes: 
   - readFileSync <- reads file synchronously (blocks the rest of the code from executing until all the data is read from a file)
-    
-    let rawdata = fs.readFileSync('data.json'); // returns raw data in a Buffer
-    let data = JSON.parse(rawdata); // parses raw data to JS Object
 
   - readFile <- asynchronous
     (err, data) => {} // ES2015 Callback fcn (once file is completely read in OS)
     err // gives err object if somuething goes wrong, else null
 */
 
-
-// fs.readFile('data.json', (err, data) => { 
-//   if (err) throw err;
-//   var toDoList = JSON.parse(data);
-//   console.log(toDoList);
-//   // var source = '{{#each data}}<li>{{this.name}}</li>{{/each}}';
-//   // var template = handlebars.compile(source);
-//   // var outputString = template(data);
-//   // // console.log(outputString);
-//   // Mustache.render('{{name}}: {{completed}}', toDoList);
-// })
-
-// var dataJson = require('data.json');
+// var source = '{{#each data}}<li>{{this.name}}</li>{{/each}}';
+// var template = handlebars.compile(source);
+// var outputString = template(data);
--- server.js
+++ server.js
@@ -3,11 +3,7 @@ var app = express();
 var path = require('path');
 
 app.get('/', (req, res) => {
-  res.send("main page");
   res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-
-console.log('This is after the read call');
 
 app.listen(3000, ()=> console.log('App listening on port 3000'));
