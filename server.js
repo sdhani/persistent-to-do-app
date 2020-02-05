335cd3c38d57e540cffc7558863f5e4cb3bdbf63
shania <shania.dhani46@cuny.edu>
2020-02-05 12:52:15 -0500

    added synchronous fs read to server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const fs = require('fs'); /* Node JS Read + Write Module */
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+let rawdata = fs.readFileSync('data.json');
 
 app.listen(3000, ()=> console.log('App listening on port 3000'));
