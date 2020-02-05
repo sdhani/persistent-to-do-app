7996a15a5b5189fee0d939683ebc88fe738a5264
shania <shania.dhani46@cuny.edu>
2020-02-05 12:44:01 -0500

    modifies global variables to constant

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
