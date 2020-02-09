ad6a1a95a5aea946e61cb8fab6193a84669c5686
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-02-09 10:22:41 -0500

    updates to reading data json

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs'); /* Node JS Read + Write Module */
+const hbs = require('hbs');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
 
 let rawdata = fs.readFileSync('data.json');
 let todoList = JSON.parse(rawdata); // convert to JSON
 
 console.log(todoList);
+
+/* Routes */
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3000, ()=> console.log('App listening on port 3000'));
