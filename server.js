dc5ec8c0594fb8b1bd9070fcc1f5439f7b25b1c4
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-02-17 11:30:23 -0500

    renamed directory

--- server.js
+++ server.js
@@ -7,12 +7,22 @@ const app = express();
 
 let data = fs.readFileSync('data.json');
 let todoList = JSON.parse(data); // convert to JSON
-
 console.log(todoList);
+
+/* Paths */
+app.set('templates', path.join(__dirname, 'templates'));
 
 /* Routes */
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
+/* 404 Error */
+app.use((req, res) => {
+  res.status(404).render('404 Error');
+});
+
 app.listen(3000, ()=> console.log('App listening on port 3000'));
+
+module.exports = app;
