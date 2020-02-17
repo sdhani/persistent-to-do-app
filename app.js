67c24457394c0aa0769feac25636ad6dab8fa178
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-02-17 15:38:02 -0500

    replaced displayList with todo component file

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
-const fs = require('fs'); /* Node JS Read + Write Module */
-const hbs = require('handlebars');
+const fs = require('fs');  /* read/write */
 
 const app = express();
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+/* Synchronized read of data JSON */
 let data = fs.readFileSync('data.json');
 let todoList = JSON.parse(data); // convert to JSON
 console.log(todoList);
 
-/* Paths */
-app.set('templates', path.join(__dirname, 'templates'));
 
 /* Routes */
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.render('todo', { todoList: todoList });
 });
 
 
 /* 404 Error */
 app.use((req, res) => {
-  res.status(404).render('404 Error');
+  res.status(404).render('404 Not Found');
+  console.log('Content requested not found.');
 });
 
 app.listen(3000, ()=> console.log('App listening on port 3000'));
