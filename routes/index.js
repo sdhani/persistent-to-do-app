1e1c52e3cd10b438257f8796210ad241265b9e4e
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-02-25 17:45:18 -0500

    working register user function, switched register routes to index file

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,46 @@
 /* routes/index.js */
 const Router = require('express').Router();
+const axios = require("axios");
+const TODO_API_URL = "https://hunter-todo-api.herokuapp.com";
 
 /* GET landing page */
 Router.get('/', async (req, res, next) => {
     try {
-      let currUser =
+      let curr_user =
         req.signedCookies.user !== undefined
           ? req.signedCookies.user
           : "Not Logged In";
-      res.render("index", { user: currUser });
-    } catch (err) {
-      console.log(err);
-    }
+      res.render('home');
+    } 
+    catch (err) { console.log(err); }
 });
 
 
-// gets user form for login
-Router.get("/login", async (req, res, next) => {
-	try {
-		res.render("login");
-	} catch (err) {
-		console.log(err);
-	}
+/* Render login page */
+Router.get('/login', async (req, res, next) => {
+  try { res.render('login'); } 
+  catch (err) { console.log(err); }
 });
 
-// gets user form for signup
-Router.get("/register", async (req, res, next) => {
-	try {
-		res.render("register-user");
-	} catch (err) {
-		console.log(err);
-	}
+/* Render register-user page */
+Router.get('/register', async (req, res, next) => {
+  try { res.render('register-user'); } 
+  catch (err) { console.log(err); }
 });
+
+/* Register User */
+Router.post('/register', async (req, res) => {
+  const { username } = req.body;
+  try{
+    const response = await axios.get(`${TODO_API_URL}/user`, {
+      username: username
+    });
+
+    console.log("Registered! :D");
+    res.status(200).json(response.data);
+  }
+  catch (err) { console.log(err); }
+});
+
 
 module.exports = Router;
