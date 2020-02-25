4f3dfd10d3b7b9b8228638bf6b1aa65a5fc0432e
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-02-25 13:31:00 -0500

    added routes for login, register, and logout

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
 const express = require('express');
+var cookieParser = require('cookie-parser');
 const exphbs = require('express-handlebars');
 const axios = require('axios');
 const fs = require('fs');  /* read/write */
-var cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(cookieParser());
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('login');
 });
+
+
+let users = { 
+  name : "Ritik", 
+  Age : "18"
+  } 
+
+
+app.
+
+app.route('/register').post((req, res) => {
+  var username = req.body.username;
+  axios.post('https://hunter-todo-api.herokuapp.com/user', {
+    username: username
+  });
+})
+
+app.route('/login').get((req, res) => {
+  var username = req.body.username;
+  res.cookie("userData", username);
+
+  axios.get(`https://hunter-todo-api.herokuapp.com/user?username=${username}`)
+  .then(console.log("user stuff", res.data));
+
+});
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('userData');
+  res.send('user logout successfully');
+});
+
+
+app.get('/setuser', (req, res) => {
+  res.cookie("userData", users);
+  res.send('user data added to cookie');
+});
+
+//Iterate users data from cookie 
+app.get('/getuser', (req, res)=>{ 
+  //shows all the cookies 
+  res.send(req.cookies); 
+}); 
+    
 
 const TODO_API_URL = 'https://hunter-todo-api.herokuapp.com';
 
@@ -24,20 +68,20 @@ app.get('/users', (req, res) => {
   });
 });
 
-app.post('/', (req, res) => {
-  axios.post(TODO_API_URL + '/user', {"username": ""});
-});
+// app.post('/', (req, res) => {
+//   axios.post(TODO_API_URL + '/user', {"username": ""});
+// });
 
-app.get('/username', (req, res) => {
-  axios.get(TODO_API_URL + '/user?username=').then((response) => {
-    res.render('todo', { todo: response.data });
-  });
-});
+// app.get('/username', (req, res) => {
+//   axios.get(TODO_API_URL + '/user?username=').then((response) => {
+//     res.render('todo', { todo: response.data });
+//   });
+// });
 
 /* 404 Error */
-app.use((req, res) => {
-  res.status(404).render('404 Not Found');
-  console.log('Content requested not found.');
+app.use('*', (req, res) => {
+  // res.status(200).send("Loaded server :)");
+  console.log("404 Not Found");
 });
 
 app.listen(3000, () => console.log('App listening on port 3000'));
