9ac1d4a6291e0ecce9081536bc47e2fe620193cf
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-02-27 20:30:21 -0500

    added auth route access

--- routes/todo.js
+++ routes/todo.js
@@ -15,5 +15,17 @@ Router.get('/', async (req, res, next) => {
   catch (err) { console.log(err); }
 });
 
+/* GET Todo-Item By ID */
+Router.get('/:id', async (req, res, next) => {
+	try{
+		const response = await axios.get(`${TODO_API_URL}/todo-item/:id`, {
+			headers: {
+				Cookie: `token=${token}`
+			}
+		});
+		res.status(200).render('todo', { todoList: response.data });
+	}
+	catch (err) { console.log(err); }
+});
 
 module.exports = Router;
