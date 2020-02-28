93d04a74f71cac99a5d1aa8e9626431916186c4d
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-02-27 21:37:01 -0500

    added add new todo item POST call

--- routes/todo.js
+++ routes/todo.js
@@ -5,7 +5,7 @@ const TODO_API_URL = "https://hunter-todo-api.herokuapp.com";
 
 Router.get('/', async (req, res, next) => {
 	const { Authentication } = req.cookies;
-	if(token) {
+	if(Authentication) {
 		try {
 			const response = await axios.get(`${TODO_API_URL}/todo-item`, { 
 				headers: {
@@ -15,15 +15,16 @@ Router.get('/', async (req, res, next) => {
 			res.status(200).render('todo', { todoList: response.data });
 		}
 		catch (err) { console.log(err); }
+
 	} else {
 		res.render('error');
 	}
 });
 
+
 /* GET Todo-Item By ID */
 Router.get('/:id', async (req, res, next) => {
 	const { Authentication } = req.cookies;
-	console.log("Req", req.params.id);
 	try {
 		const response = await axios.get(`${TODO_API_URL}/todo-item/${req.params.id}`, {
 			headers: {
@@ -35,10 +36,21 @@ Router.get('/:id', async (req, res, next) => {
 	catch (err) { console.log(err); }
 });
 
+
+
 /* POST New Todo-Item */
 Router.post('/', async(req, res, next) => {
 	const { Authentication } = req.cookies;
-	
+	const { content } = req.body;
+	try {
+		const response = await axios.post(`${TODO_API_URL}/todo-item`, { content: content }, {
+			headers: {
+				Cookie: `token=${Authentication}`
+			}
+		});
+		res.status(200).redirect('/todo');
+	}
+	catch (err) { console.log(err); }
 });
 
 module.exports = Router;
