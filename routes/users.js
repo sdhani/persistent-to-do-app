f6c18332e6443c9b8c724b9943f4f0a18da0658b
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-03-03 11:51:36 -0500

    modified GET all users route logic; cleaned up code

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,16 @@
 /* routes/users.js */
-
 const Router = require("express").Router();
 const axios = require("axios");
 const TODO_API_URL = "https://hunter-todo-api.herokuapp.com";
 
+/* GET all users on server */
 Router.get("/", async (req, res) => {
 	try {
-		const curr_user = await axios.get(`${TODO_API_URL}/user`);
-		res.status(200).json(curr_user.data);
+		const response = await axios.get(`${TODO_API_URL}/user`);
+		res.status(200).render('user-list', { users: response.data });
   }
   catch(err) { console.log(err); }
 });
 
 
-Router.get("/:username", async(req,res)=>{
-	try{
-		const username = await axios.get(`${TODO_API_URL}/user?username=${req.params.username}`) 
-		res.status(200).json(username.data);
-  }
-  catch(err){ console.log(err); }
-});
-
-
-module.exports = Router;
+module.exports = Router; /* export Router */
